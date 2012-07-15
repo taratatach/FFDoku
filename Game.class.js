824baa7be29a392b07b6aac6982b4e9305113656
Guyader Erwan <erwanguyader@gmail.com>
2012-07-15 23:00:25 +0200

    On peut désormais modifier le chiffre d'une case

--- Game.class.js
+++ Game.class.js
@@ -1,14 +1,56 @@
 function Game() {
-	this.cells;
+	this.grid = new Array(9);
+	for (i=0; i<9; i++) {
+		this.grid[i] = new Array(9);
+		for (j=0; j<9; j++)
+		  this.grid[i][j] = null;
+  }
 }
 
-Game.prototype.constructor = Game;
-function Game() {
-	this.cells = new Array(9);
-	for (i=0; i<9; i++)s
-		cells[i] = new Array(9);
+
+Game.prototype.newGame = function () {
+	this.generateGrid();
 }
 
-Game.prototype.reset = function () {
+Game.prototype.respect = function (nb, l, c) {
+  for (i = 0; i < 9; i++) {
+    //console.log('i=' + i + ', c=' + c + ', grid[' + l + '][' + i + ']=' + this.grid[l][i] + ', nb=' + nb);
+	  if (i != c && this.grid[l][i] == nb) {
+	    //console.log('pas bon');
+	    return false;
+		}
+	}
+		 	
+	for (i = 0; i < 9; i++)
+	  if (i != l && this.grid[i][c] == nb)
+		 	return false;
+	
+	var i = (c % 3 == 0) ? c : ((c % 3 == 1) ? c - 1 : c - 2);
+	var ibis = i + 3;
+	var j = (l % 3 == 0) ? l : ((l % 3 == 1) ? l - 1 : l - 2);
+	var jbis = j + 3;
+	
+	for (; i < ibis; i++)
+		for (; j < jbis; j++)
+			if ((i != l || j != c) && this.grid[i][j] == nb)
+				return false;
+	
+	return true;
+}
 
+Game.prototype.generateGrid = function () {
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			var c = null;
+			do {
+				c = 1 + Math.floor(Math.random() * 9);
+			} while (this.respect(c, i, j) == false);
+			
+			this.grid[i][j] =  c;
+		}
+	}
+}
+
+Game.prototype.solve = function () {
+  
 }
--- Grid.class.js
+++ Grid.class.js
@@ -1,12 +1,12 @@
-function Grid() {
-	this.cells = null;
-	this.newGameLink = null;
-	this.reset = null;
-}
+/* SITES INTERESSANTS
+ *
+ * http://killdream.github.com/blog/2011/10/understanding-javascript-oop/
+ */
 
-Grid.prototype.constructor = Grid;
-function Grid() {
+function Grid (game) {
 	var table = document.getElementById('grid');
+	this.commands = document.getElementById('commands');
+	this.commands.className = 'hidden';
 	
 	/* Initialize attributes */
 	this.cells = new Array(9);
@@ -17,43 +17,104 @@ function Grid() {
 		this.cells[i] = new Array(9);
 		for (j=0; j<9; j++) {
 			var td = document.createElement('td');
-			td.setAttribute('class', 'cell');
+			
+			if (game.grid[i][j] == null)
+  			td.className = 'cell';
+	    else {
+	      td.className = 'number';
+	      td.innerHTML = game.grid[i][j];
+	    }
+	    
 			tr.appendChild(td);
 			
 			this.cells[i][j] = td;
 		}			
 	}
-		
-	this.newGameLink = document.getElementById('newGame');
-	
+	this.newGame = document.getElementById('newGame');
 	this.reset = document.getElementById('reset');
+	this.modifiedCell = null;
+	this.inserts = document.getElementsByClassName('insert');
+	this.erase = document.getElementById('erase');
 	
 	/* Add events */
-	Grid.prototype.addEvents();
-}
-
-Grid.prototype.fillCell = function (number, cell) {
-	cell.className = 'number';
-	cell.nodeValue = String(number);
+	this.addEvents();
 }
 
 Grid.prototype.addEvents = function () {
-	for(i=0; i<this.cells.length; i++)
-  	this.cells[i].addEventListener('click', Grid.prototype.getEvtCell, false);
-  	
-  this.newGameLink.addEventListener('click', Grid.prototype.getEvtNewGame, false);
-  	  	
-  this.reset.addEventListener('click', Grid.prototype.getEvtReset, false);
+	var self = this;
+	
+	document.addEventListener('click', 
+		function(e) {
+			self.getEvtBody.call(self, e)
+		},
+	true);
+	
+	var cells = document.getElementsByClassName('cell');
+	for(i=0; i<cells.length; i++) {
+		cells[i].addEventListener('click',
+			function(e) {
+				self.getEvtClickCell.call(self, e)
+			}, 
+		false);
+		cells[i].addEventListener('change',
+			function(e) {
+				self.getEvtChangeCell.call(self, e)
+			},
+		false);
+	}
+	
+  this.newGame.addEventListener('click', self.getEvtNewGame, false);
+  
+  this.reset.addEventListener('click', self.getEvtReset, false);
+  
+	for(i=0; i<this.inserts.length; i++)
+		this.inserts[i].addEventListener('click',
+			function(e) {
+				self.getEvtInsert.call(self, e)
+			},
+		false);
+	
+	this.erase.addEventListener('click',
+	  function() {
+	    self.getEvtErase.call(self)
+	  },
+	false);
 }
 
-Grid.prototype.getEvtCell = function (e) {
-	alert('bouh');
+Grid.prototype.getEvtBody = function (e) {
+	if (e.target.nodeName != 'TD')
+		this.commands.className = 'hidden';
 }
+
+Grid.prototype.getEvtClickCell = function (e) {
+	console.log(this);
+	this.modifiedCell = e.target;
+	if (this.commands.className == 'hidden')
+		this.commands.className = 'displayed';
+	else
+		this.commands.className = 'hidden';
+}
+
+Grid.prototype.getEvtChangeCell = function (e) {
+	// check if value inserted is correct
+	
+	// otherwise change background to red
+}
+
+Grid.prototype.getEvtInsert = function (e) {
+	this.modifiedCell.innerHTML = e.target.innerHTML;
+	this.commands.className = 'hidden';
+}
+
+Grid.prototype.getEvtErase = function() {
+  this.modifiedCell.innerHTML = '';
+  this.commands.className = 'hidden';
+ }
 
 Grid.prototype.getEvtNewGame = function (e) {
-
+	alert('new game');
 }
 
 Grid.prototype.getEvtReset = function (e) {
-
+	alert('reset');
 }
